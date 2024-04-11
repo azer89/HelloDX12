e60b746e8e81c33ccb7125581c9811304490f331
Reza Saputra <radhitya@uwaterloo.ca>
2024-04-11 14:14:17 -0400

    Pipeline Functions

--- HelloDX12/Header/PipelineSimple.h
+++ HelloDX12/Header/PipelineSimple.h
@@ -12,12 +12,22 @@ public:
 
 	void PopulateCommandList(DX12Context& ctx);
 
+private:
+	void CreateSRV(DX12Context& ctx);
+	void CreateRTV(DX12Context& ctx);
+	void CreateRootSignature(DX12Context& ctx);
+	void CreateShaders(DX12Context& ctx);
+	void CreateGraphicsPipeline(DX12Context& ctx);
+
 public:
 	ComPtr<ID3D12PipelineState> pipelineState_;
 	ComPtr<ID3D12RootSignature> rootSignature_;
 
-	ComPtr<ID3D12DescriptorHeap> rtvHeap_;
-	ComPtr<ID3D12DescriptorHeap> srvHeap_;
+	ComPtr<ID3DBlob> vertexShader_;
+	ComPtr<ID3DBlob> pixelShader_;
+
+	ComPtr<ID3D12DescriptorHeap> rtvHeap_; // Render target views
+	ComPtr<ID3D12DescriptorHeap> srvHeap_; // Shader resource views
 	
 	UINT rtvDescriptorSize_;
 	ComPtr<ID3D12Resource> renderTargets_[DX12Context::FrameCount];
