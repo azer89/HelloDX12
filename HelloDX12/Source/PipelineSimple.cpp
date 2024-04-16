39cc504aefa978a4b92ec88eed5f71637093139b
Reza Saputra <radhitya@uwaterloo.ca>
2024-04-16 13:01:36 -0400

    Cull mode

--- HelloDX12/Source/PipelineSimple.cpp
+++ HelloDX12/Source/PipelineSimple.cpp
@@ -203,6 +203,7 @@ void PipelineSimple::CreateGraphicsPipeline(DX12Context& ctx)
 	};
 	psoDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM;
 	psoDesc.SampleDesc.Count = 1;
+	psoDesc.RasterizerState.CullMode = D3D12_CULL_MODE_NONE;
 	ThrowIfFailed(ctx.GetDevice()->CreateGraphicsPipelineState(&psoDesc, IID_PPV_ARGS(&pipelineState_)));
 }
 
