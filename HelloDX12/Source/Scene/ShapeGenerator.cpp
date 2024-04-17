1875fc1cf4cef6b7e99dd55068e99f384a4fe43b
Reza Saputra <radhitya@uwaterloo.ca>
2024-04-17 04:34:02 -0400

    Cleanup

--- HelloDX12/Source/Scene/ShapeGenerator.cpp
+++ HelloDX12/Source/Scene/ShapeGenerator.cpp
@@ -4,37 +4,37 @@ void ShapeGenerator::Cube(std::vector<VertexData>& vertices, std::vector<uint32_
 {
 	vertices =
 	{
-		// TOP: normal aims up (positive y-axis) in local space
+		// Top: normal aims up (positive y-axis) in local space
 		{ glm::vec3(-1.0f, 1.0f, -1.0f), glm::vec3(0.0f, 1.0f, 0.0f), glm::vec2(0.0f, 1.0f) },
 		{ glm::vec3(1.0f,  1.0f, -1.0f), glm::vec3(0.0f, 1.0f, 0.0f), glm::vec2(1.0f, 1.0f) },
 		{ glm::vec3(1.0f,  1.0f,  1.0f), glm::vec3(0.0f, 1.0f, 0.0f), glm::vec2(1.0f, 0.0f) },
 		{ glm::vec3(-1.0f, 1.0f,  1.0f), glm::vec3(0.0f, 1.0f, 0.0f), glm::vec2(0.0f, 0.0f) },
 
-		// BOTTOM: normal aims down (negative y-axis) in local space
+		// Bottom: normal aims down (negative y-axis) in local space
 		{ glm::vec3(-1.0f, -1.0f, -1.0f), glm::vec3(0.0f, -1.0f, 0.0f), glm::vec2(1.0f, 1.0f) },
 		{ glm::vec3( 1.0f, -1.0f, -1.0f), glm::vec3(0.0f, -1.0f, 0.0f), glm::vec2(0.0f, 1.0f) },
 		{ glm::vec3( 1.0f, -1.0f,  1.0f), glm::vec3(0.0f, -1.0f, 0.0f), glm::vec2(0.0f, 0.0f) },
 		{ glm::vec3(-1.0f, -1.0f,  1.0f), glm::vec3(0.0f, -1.0f, 0.0f), glm::vec2(1.0f, 0.0f) },
 
-		// LEFT: normal aims right (negative x-axis) in local space
+		// Left: normal aims right (negative x-axis) in local space
 		{ glm::vec3(-1.0f, -1.0f,  1.0f), glm::vec3(-1.0f, 0.0f, 0.0f), glm::vec2(0.0f, 1.0f) },
 		{ glm::vec3(-1.0f, -1.0f, -1.0f), glm::vec3(-1.0f, 0.0f, 0.0f), glm::vec2(1.0f, 1.0f) },
 		{ glm::vec3(-1.0f,  1.0f, -1.0f), glm::vec3(-1.0f, 0.0f, 0.0f), glm::vec2(1.0f, 0.0f) },
 		{ glm::vec3(-1.0f,  1.0f,  1.0f), glm::vec3(-1.0f, 0.0f, 0.0f), glm::vec2(0.0f, 0.0f) },
 
-		// RIGHT: normal aims right (positive x-axis) in local space
+		// Right: normal aims right (positive x-axis) in local space
 		{ glm::vec3(1.0f, -1.0f,  1.0f), glm::vec3(1.0f, 0.0f, 0.0f), glm::vec2(1.0f, 1.0f) },
 		{ glm::vec3(1.0f, -1.0f, -1.0f), glm::vec3(1.0f, 0.0f, 0.0f), glm::vec2(0.0f, 1.0f) },
 		{ glm::vec3(1.0f,  1.0f, -1.0f), glm::vec3(1.0f, 0.0f, 0.0f), glm::vec2(0.0f, 0.0f) },
 		{ glm::vec3(1.0f,  1.0f,  1.0f), glm::vec3(1.0f, 0.0f, 0.0f), glm::vec2(1.0f, 0.0f) },
 
-		// FRONT: normal aims forwards (negative z-axis) in local space
+		// Front: normal aims forwards (negative z-axis) in local space
 		{ glm::vec3(-1.0f, -1.0f, -1.0f), glm::vec3(0.0f, 0.0f, -1.0f), glm::vec2(0.0f, 1.0f) },
 		{ glm::vec3( 1.0f, -1.0f, -1.0f), glm::vec3(0.0f, 0.0f, -1.0f), glm::vec2(1.0f, 1.0f) },
 		{ glm::vec3( 1.0f,  1.0f, -1.0f), glm::vec3(0.0f, 0.0f, -1.0f), glm::vec2(1.0f, 0.0f) },
 		{ glm::vec3(-1.0f,  1.0f, -1.0f), glm::vec3(0.0f, 0.0f, -1.0f), glm::vec2(0.0f, 0.0f) },
 
-		// BACK: normal aims backwards (positive z-axis) in local space
+		// Back: normal aims backwards (positive z-axis) in local space
 		{ glm::vec3(-1.0f, -1.0f, 1.0f), glm::vec3(0.0f, 0.0f, 1.0f), glm::vec2(1.0f, 1.0f) },
 		{ glm::vec3( 1.0f, -1.0f, 1.0f), glm::vec3(0.0f, 0.0f, 1.0f), glm::vec2(0.0f, 1.0f) },
 		{ glm::vec3( 1.0f,  1.0f, 1.0f), glm::vec3(0.0f, 0.0f, 1.0f), glm::vec2(0.0f, 0.0f) },
@@ -43,28 +43,28 @@ void ShapeGenerator::Cube(std::vector<VertexData>& vertices, std::vector<uint32_
 
 	indices =
 	{
-		// TOP
-		3,1,0,
-		2,1,3,
+		// Top
+		3, 1, 0,
+		2, 1, 3,
 
-		// BOTTOM
-		6,4,5,
-		7,4,6,
+		// Bottom
+		6, 4, 5,
+		7, 4, 6,
 
-		// LEFT
-		11,9,8,
-		10,9,11,
+		// Left
+		11, 9, 8,
+		10, 9, 11,
 
-		// RIGHT
-		14,12,13,
-		15,12,14,
+		// Right
+		14, 12, 13,
+		15, 12, 14,
 
-		// FRONT
-		19,17,16,
-		18,17,19,
+		// Front
+		19, 17, 16,
+		18, 17, 19,
 
-		// BACK
-		22,20,21,
-		23,20,22
+		// Back
+		22, 20, 21,
+		23, 20, 22
 	};
 }
