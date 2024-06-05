cf1aae2b4ee8c0ca3c3772b4a65ed38e25953448
Reza Saputra <radhitya@uwaterloo.ca>
2024-06-05 18:39:22 -0400

    wip

--- HelloDX12/Source/Resources/ResourcesIBL.cpp
+++ HelloDX12/Source/Resources/ResourcesIBL.cpp
@@ -1,6 +1,7 @@
 #include "ResourcesIBL.h"
 #include "PipelineEquirect2Cube.h"
 #include "PipelineBRDFLUT.h"
+#include "Utility.h"
 
 constexpr uint32_t CUBE_SIZE = 1024;
 
@@ -9,9 +10,17 @@ ResourcesIBL::ResourcesIBL(DX12Context& ctx, const std::string& hdrFile)
 	hdrImage_.LoadHDR(ctx, hdrFile);
 	hdrImage_.buffer_.SetName("HDR_Image");
 
-	constexpr uint32_t mipmapCount = 1;
-	environmentCubemap_.CreateCubemap(ctx, CUBE_SIZE, CUBE_SIZE, mipmapCount);
+	constexpr uint32_t envMipmapCount = 1;
+	environmentCubemap_.CreateCubemap(ctx, CUBE_SIZE, CUBE_SIZE, envMipmapCount);
 	environmentCubemap_.buffer_.SetName("Environment_Cubemap");
+
+	constexpr uint32_t diffuseMipmapCount = 1;
+	diffuseCubemap_.CreateCubemap(ctx, CUBE_SIZE, CUBE_SIZE, diffuseMipmapCount);
+	diffuseCubemap_.buffer_.SetName("Diffuse_Cubemap");
+
+	const uint32_t specularMipmapCount = Utility::MipMapCount(CUBE_SIZE);
+	specularCubemap_.CreateCubemap(ctx, CUBE_SIZE, CUBE_SIZE, specularMipmapCount);
+	specularCubemap_.buffer_.SetName("Specular_Cubemap");
 
 	brdfLutImage_.Create(ctx, 256, 256, 1, 1, DXGI_FORMAT_R16G16_FLOAT, D3D12_RESOURCE_FLAG_ALLOW_UNORDERED_ACCESS);
 	brdfLutImage_.buffer_.SetName("BRDF_LUT");
@@ -32,5 +41,7 @@ void ResourcesIBL::Destroy()
 {
 	hdrImage_.Destroy();
 	environmentCubemap_.Destroy();
+	diffuseCubemap_.Destroy();
+	specularCubemap_.Destroy();
 	brdfLutImage_.Destroy();
 }
