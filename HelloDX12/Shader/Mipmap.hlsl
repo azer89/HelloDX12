22b5a3a489d1236eab09b2370dfacb32b5ffb78c
Reza Saputra <radhitya@uwaterloo.ca>
2024-06-08 17:45:27 -0400

    wip

--- HelloDX12/Shader/Mipmap.hlsl
+++ HelloDX12/Shader/Mipmap.hlsl
@@ -5,7 +5,7 @@ Downsample shader for generating mipmap
 
 Texture2D<float4> srcTexture : register(t0);
 RWTexture2D<float4> dstTexture : register(u0);
-SamplerState bilinearClamp : register(s0);
+SamplerState defaultSampler : register(s0);
 
 cbuffer CB : register(b0)
 {
@@ -20,7 +20,7 @@ void CSMain(uint3 threadID : SV_DispatchThreadID)
     float2 texcoords = texelSize * (threadID.xy + 0.5);
 
 	// The samplers linear interpolation will mix the four pixel values to the new pixels color
-    float4 color = srcTexture.SampleLevel(bilinearClamp, texcoords, 0);
+    float4 color = srcTexture.SampleLevel(defaultSampler, texcoords, 0);
 
 	// Write the final color into the destination texture
     dstTexture[threadID.xy] = color;
